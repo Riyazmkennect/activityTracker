756301d3e570aba322a53099bd925264c40efc65
Syed Riyaz <riyazulahad786@gmail.com>
2022-06-18 12:41:26 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <h1>Track your Activities</h1>
+    <p>Hey user Hope you use this app in best way!</p>
+
+    <div class="create-new">
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="hey user add your activity"
+        @keypress.enter="addTask"
+        
+      />
+      <button @click="addTask">Add</button>
+    </div>
+
+    <div class="tasks">
+     <!-- {{$store.state.tasks}} -->
+     <Task v-for="(task,i) in $store.state.tasks " :key="i" :task="task"/>
+    </div>
+    
+  </main>
+
+</template>
+
+<script>
+export default {
+  // name: "IndexPage",
+  data(){
+    return {
+      newTask:""
+    }
+   
+  },
+  methods:{
+    addTask(){
+      if(this.newTask){
+        this.$store.commit('ADD_TASK',this.newTask)
+        this.newTask=""
+      }
+    }
+  }
+};
+</script>
---
+++ pages/uiDesign.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button class="   btn mt-12"> submit
+            <v-icon>email</v-icon>
+        </button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.btn{
+    align-content: center;
+    position: relative;
+    color: blue; 
+    appearance: none;
+	border: none;
+	outline: none;
+	background-color: rgb(47, 214, 111);
+	text-transform: uppercase;
+	font-weight: bold;
+	color: #000;
+	font-size: 0.875rem;
+	padding: 0.5rem 1rem;
+	border-radius: 0.5rem;
+	margin-left: 0.5rem;
+	cursor: pointer;
+
+}
+</style>
